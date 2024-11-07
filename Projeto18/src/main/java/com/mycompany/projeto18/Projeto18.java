9169cc09907760c898670046f439cb792e976075
Franquilei <franquilei.oliveira@aluno.senai.br>
2024-11-06 21:34:50 -0300

    Add files via upload

---
+++ Projeto18/src/main/java/com/mycompany/projeto18/Projeto18.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto18;
+
+/**
+ *
+ * @author f.oliveira
+ */
+public class Projeto18 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
